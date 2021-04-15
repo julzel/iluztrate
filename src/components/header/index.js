5488b02b6240b48e901a574767fff7f5b6208512
julzel <julio@zarmada.com>
2021-04-15 16:24:20 -0600

    updated header for home

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -4,13 +4,13 @@ import { withRouter } from 'react-router-dom';
 
 import './Header.scss';
 import Logo from '../../assets/logo_white.svg';
-import ColorLogo from '../../assets/logo_colores.svg';
+import BlackLogo from '../../assets/logo_black.svg';
 
 const Header = ({ colorLogo, history }) => {
   return (
     <header className={`header${colorLogo ? ' no-bg' : ''}`}>
       <div className="header-logo" onClick={() => history.push('/inicio')}>
-        <Svg src={colorLogo ? ColorLogo : Logo} />
+        <Svg src={colorLogo ? BlackLogo : Logo} />
       </div>
       <div className="header-menu-icon">
         <i className="fa fa-bars" style={{color: colorLogo ? '#126168' : '#ffffff'}} />
